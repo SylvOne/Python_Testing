2fd25d6dec4c1f0a28bafdce25f3585eba3fbc16
sylv1un <sylv1un@gmail.com>
2023-06-30 09:18:39 +0200

    Tests : Performance

---
+++ tests/tests_performance/locustfile.py
@@ -0,0 +1,38 @@
+from locust import HttpUser, task, between
+
+
+class ProjectPerfTest(HttpUser):
+    wait_time = between(5, 15)  # Temps d'attente entre les taches/utilisateurs
+
+    @task
+    def club_table(self):
+        self.client.get("/points", name="Load club points")
+
+    @task
+    def on_start(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"}, name="Show summary")
+
+    @task
+    def perf_index(self):
+        self.client.get("/", name="Load index")
+
+    @task
+    def booking(self):
+        self.client.get("/book/Fall Classic/She Lifts", name="Book Fall Classic She Lifts")
+        self.client.get("/book/Spring Festival/Iron Temple", name="Book Spring Festival Iron Temple")
+
+    @task
+    def purchase_places(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                "competition": "Spring Festival",
+                "club": "Iron Temple",
+                "places": 4,
+            },
+            name="Buy and update points"
+        )
+
+    @task
+    def on_stop(self):
+        self.client.get("/logout", name="Logout")
